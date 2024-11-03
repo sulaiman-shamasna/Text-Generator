172e32a1df9b6deb434cf5c1b603368813d0f3d7
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-11-03 22:52:24 +0100

    chore: :building_construction: Add a helper function to read and export OPENAI_API_KEY

--- helper.py
+++ helper.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-# these expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
+# These expect to find a .env file at the directory above the lesson.                                                                                                                     # the format for that file is (without the comment)                                                                                                                                       #API_KEYNAME=AStringThatIsTheLongAPIKeyFromSomeService                                                                                                                                     
 def load_env():
     _ = load_dotenv(find_dotenv())
 
